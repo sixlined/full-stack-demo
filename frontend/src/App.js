a3c07ffb568c3e54f76a7a4f9da164c7be13954f
Kaleb Tringale <kaleb.tringale@gmail.com>
2025-05-08 12:17:32 -0500

    pushing up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>I edited the page. Ha!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		console.log("increase");
+	}, [count]);
+
+	const MyVariable = ({ text }) => {
+		const myVariable = "Hello World!";
+		return (
+			<div>
+				<button onClick={() => setCount(count + 1)}>Increment</button>
+				<p>my variable is {count}</p>
+			</div>
+		);
+	};
+
+	const Nav = ({ text }) => {
+		const myVariable = "Hello World!";
+		return (
+			<li>
+				<a href="#">Filthy Prank</a>
+				<a href="#">Filthy Prank</a>
+				<a href="#">Filthy Prank</a>
+				<a href="#">Filthy Prank</a>
+			</li>
+		);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<Nav />
+				<MyVariable text="Good mythical morning" />
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
